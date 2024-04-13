c621f64c16291f4b61d5d89aaea182625b329745
Amar AA <amar.ashfaq95@gmail.com>
2024-04-13 19:40:32 +0100

    From using promises to async await. Just another way of doing things in a more efficient manner

--- TestingAPIScript.js
+++ TestingAPIScript.js
@@ -1,49 +1,45 @@
 
-// POST method
-fetch('https://jsonplaceholder.typicode.com/posts', {
-	method: 'POST',
-	body: JSON.stringify({
-		title: 'Rules rule the world',
-		body: 'Play nice everyone',
-		userId: 58,
-	}),
-	headers: {
-		'Content-type': 'application/json; charset=UTF-8',
-	},
-})
-.then((response) => {
-	return response.json();
-})
-.then((post) => {
-	console.log(post);
-	console.log(post.title);
-});
-
-// default - GET method
-fetch('https://jsonplaceholder.typicode.com/posts/')
-	.then((response) => {
-		return response.json();
-	})
-	.then((response) => {		
-		response.forEach(item => {
+// async await call - POST
+async function createPost() {
+	
+	try {
+		const postData = {
+			title: 'Rules rule the world',
+			body:'Play nice everyone',
+			userId: 58
+		};
+	
+		const response = await fetch('https://jsonplaceholder.typicode.com/posts/', {
+			method: 'POST',
+			body: JSON.stringify(postData),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+			}			
+		});
+		
+		const post = await response.json();
+		console.log(post);
+		console.log(post.title);
+	}
+	catch (error) {
+		console.error('Error creating data: ', error);
+	}
+}
+createPost();
+	
+// async await call - GET
+async function fetchData() {
+	
+	try {
+		const response = await fetch('https://jsonplaceholder.typicode.com/posts/');
+		const data = await response.json();
+		
+		data.forEach(item => {
 			console.log(item);
-		});				
-	})
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		});
+	}
+	catch (error) {
+		console.error('Error fetching data:', error);
+	}
+}
+fetchData();
